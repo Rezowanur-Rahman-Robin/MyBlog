19b8d4f1c9911b84ed3638819f2eb31f15d5b17a
Ratul1997 <robincuetcse@gmail.com>
2022-06-12 19:33:50 +0600

    bug fix

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
 if(process.env.NODE_ENV ==="production"){
     app.use(express.static('frontend/build'))
-    app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+//     app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   )
 }
 
 app.use(notFound)//middleware
