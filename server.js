aa9144c4b91c9f76ec2fcea43c7ef6a93b9f3660
Ratul1997 <robincuetcse@gmail.com>
2022-06-12 20:28:42 +0600

    fix

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 //     res.send(`Api is running properly on port:${port}`)
 // })
 console.log(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-console.log(express.static('frontend/build'))
+console.log(express.static(path.join(__dirname, '/frontend/build')))
 
 if(process.env.NODE_ENV ==="production"){
-    // app.use(express.static('frontend/build'))
+    app.use(express.static('frontend/build'))
     app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 
