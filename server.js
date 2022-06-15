60892dad70139cc1532684007e01b772c1a2e810
Ratul1997 <robincuetcse@gmail.com>
2022-06-15 21:53:49 +0600

    Post deleting bug fixed

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ const __dirname = path.resolve()
 
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
-app.get('/',(req,res)=>{
-    res.send(`Api is running properly on port:${port}`)
-})
+// app.get('/',(req,res)=>{
+//     res.send(`Api is running properly on port:${port}`)
+// })
 // console.log(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 // console.log(express.static(path.join(__dirname, '/frontend/build')))
 
