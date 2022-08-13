953c7bf6755017ee69caf6e6495c2c573ab94b05
Ratul1997 <robincuetcse@gmail.com>
2022-08-13 16:04:56 +0600

    4 new blog added

--- server.js
+++ server.js
@@ -1,43 +1,36 @@
-import express from 'express';
-import path from 'path';
-import blogRoutes from './routes/blogRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import path from "path";
+import blogRoutes from "./routes/blogRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 
-
-
-dotenv.config()
+dotenv.config();
 
 connectDB();
 
-
-
-
 const app = express();
 
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 //allow us to post json data on the body.
 
 const port = process.env.PORT || 5009;
 
-app.use('/api/posts',blogRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/upload',uploadRoutes)
-
+app.use("/api/posts", blogRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
 
 //for making the upload folder static
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 // console.log(path.join(__dirname,'../uploads'))
 
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // app.get('/',(req,res)=>{
 //     res.send(`Api is running properly on port:${port}`)
@@ -45,17 +38,15 @@ app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 // console.log(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 // console.log(express.static(path.join(__dirname, '/frontend/build')))
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static('frontend/build'))
-    app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-
-  )
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 }
 
-app.use(notFound)//middleware
+app.use(notFound); //middleware
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-app.listen(port, console.log(`Server running: ${port}`))
+app.listen(port, console.log(`Server running: ${port}`));
